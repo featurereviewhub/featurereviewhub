0488331f01dca52e61647b6543dcfbe4f677e82c
featurereviewhub <ABDESSAMADOSSEILI2003@gmail.com>
2024-07-09 05:32:59 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,9 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/featurereviewhub-favicon-black.png" alt="Feature ReviewHub Logo" className={styles.logo} />
       </footer>
     </>
   )
 }
+
