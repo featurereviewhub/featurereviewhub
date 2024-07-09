71f66e32816579431f8123543133cb377d6f1bb5
featurereviewhub <ABDESSAMADOSSEILI2003@gmail.com>
2024-07-09 05:29:43 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,27 @@ export default function Home() {
       <main>
         <Header title="HEY!" />
         <p className="description">
-          Get started shopping at <a href="https://featurereviewhub.com">FEATUREREVIEWHUB</a>
+          Get started shopping at 
+          <a href="https://featurereviewhub.com" className="link">
+            FEATUREREVIEWHUB
+            <svg className="arrow" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M13 4L20 12L13 20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M4 12H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+          </a>
         </p>
       </main>
 
       <Footer />
+      <style jsx>{`
+        .link {
+          display: inline-flex;
+          align-items: center;
+        }
+        .arrow {
+          margin-left: 5px;
+        }
+      `}</style>
     </div>
   )
 }
