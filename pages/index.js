98794b29be855b882ece7f5a5529565c35c850e7
featurereviewhub <ABDESSAMADOSSEILI2003@gmail.com>
2024-07-18 18:07:52 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,86 +1,21 @@
- Redirecting...
-    <link rel="icon" href="../sf-tb-sg.ibytedtos.com/obj/eden-sg/uhtyvueh7nulogpoguhm/tiktok-icon2.png" type="image/x-icon" />
-    <script type="text/javascript" src="#" charset="UTF-8"></script><style>
-        body {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            background-color: #f0f0f0;
-            font-family: Arial, sans-serif;
-        }
+ import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-        #tiktok-logo {
-            width: 250px;
-            margin-bottom: 20px;
-        }
+export default function Home() {
+  const router = useRouter();
 
-        #redirect-text {
-            font-size: 30px;
-            color: #333;
-            opacity: 0;
-            animation: fadeIn 2s ease-in-out forwards, scaleUp 2s ease-in-out infinite alternate;
-        }
+  useEffect(() => {
+    // Redirect to your desired URL
+    window.location.href = 'https://smrturl.co/5274e7d';
 
-        @keyframes fadeIn {
-            0% {
-                opacity: 0;
-            }
+    // Optionally, you can use router.push() instead for client-side routing
+    // router.push('https://featurereviewhub.com/');
+  }, []);
 
-            100% {
-                opacity: 1;
-            }
-        }
-
-        @keyframes scaleUp {
-            0% {
-                transform: scale(1);
-            }
-
-            100% {
-                transform: scale(1.2);
-            }
-        }
-
-        #openLink {
-            margin: 50px;
-            text-align: center;
-        }
-    </style>
-<link rel="me" href="https://www.blogger.com/profile/15640536915796453344" />
-
-
-
-
-
-    <img id="tiktok-logo" src="../i.ibb.co/5MMGFNf/1000-F-344600545-4j-FBe-Ak5l8-Kug2-Uyxq-OEoop8p1-I0v1dp-removebg-preview.png" alt="TikTok Logo" />
-    <p id="redirect-text">Being Redirected ...</p>
-    <a href="www.google.html" id="openLink">Click Me to Open in Chrome (Android) or Safari (iOS)</a>
-
-    <script>
-        function openLinkBasedOnDevice(url) {
-            var userAgent = navigator.userAgent;
-
-            if (/(android)/i.test(userAgent)) {
-                // If the device is Android, open the link in Chrome
-                window.location.href = 'intent://' + url.substring(url.indexOf("://") + 3) + '#Intent;scheme=http;package=com.android.chrome;end;';
-            } else if (/(iphone|ipad|ipod)/i.test(userAgent)) {
-                // If the device is iOS, open the link in Safari
-                window.location.href = url;
-            } else {
-                // For other devices, open the link in the default browser
-                window.open(url, '_blank');
-            }
-        }
-
-        // Automatically execute the function after a 5-second delay
-        setTimeout(function () {
-            var url = 'https://featurereviewhub.com/';
-            openLinkBasedOnDevice(url);
-        }, 500); // 500 milliseconds = 5 seconds
-    </script>
-
-
-</!doctype>
+  return (
+    <div>
+      <p>Redirecting...</p>
+      {/* You can optionally add a message or spinner here */}
+    </div>
+  );
+}
