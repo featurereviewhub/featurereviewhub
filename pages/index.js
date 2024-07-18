7b5c62c3f9e316f493be124d02634e19b40f1022
featurereviewhub <ABDESSAMADOSSEILI2003@gmail.com>
2024-07-18 18:04:51 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Redirect Example</title>
-</head>
-<body>
-    <img id="tiktok-logo" src="https://i.ibb.co/5MMGFNf/1000-F-344600545-4j-FBe-Ak5l8-Kug2-Uyxq-OEoop8p1-I0v1dp-removebg-preview.png" alt="TikTok Logo" />
+ Redirecting...
+    <link rel="icon" href="../sf-tb-sg.ibytedtos.com/obj/eden-sg/uhtyvueh7nulogpoguhm/tiktok-icon2.png" type="image/x-icon" />
+    <script type="text/javascript" src="#" charset="UTF-8"></script><style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #f0f0f0;
+            font-family: Arial, sans-serif;
+        }
+
+        #tiktok-logo {
+            width: 250px;
+            margin-bottom: 20px;
+        }
+
+        #redirect-text {
+            font-size: 30px;
+            color: #333;
+            opacity: 0;
+            animation: fadeIn 2s ease-in-out forwards, scaleUp 2s ease-in-out infinite alternate;
+        }
+
+        @keyframes fadeIn {
+            0% {
+                opacity: 0;
+            }
+
+            100% {
+                opacity: 1;
+            }
+        }
+
+        @keyframes scaleUp {
+            0% {
+                transform: scale(1);
+            }
+
+            100% {
+                transform: scale(1.2);
+            }
+        }
+
+        #openLink {
+            margin: 50px;
+            text-align: center;
+        }
+    </style>
+<link rel="me" href="https://www.blogger.com/profile/15640536915796453344" />
+
+
+
+
+
+    <img id="tiktok-logo" src="../i.ibb.co/5MMGFNf/1000-F-344600545-4j-FBe-Ak5l8-Kug2-Uyxq-OEoop8p1-I0v1dp-removebg-preview.png" alt="TikTok Logo" />
     <p id="redirect-text">Being Redirected ...</p>
-    <a href="https://www.google.com" id="openLink">Click Me to Open in Chrome (Android) or Safari (iOS)</a>
+    <a href="www.google.html" id="openLink">Click Me to Open in Chrome (Android) or Safari (iOS)</a>
 
     <script>
         function openLinkBasedOnDevice(url) {
@@ -30,7 +79,8 @@
         setTimeout(function () {
             var url = 'https://featurereviewhub.com/';
             openLinkBasedOnDevice(url);
-        }, 5000); // 5000 milliseconds = 5 seconds
+        }, 500); // 500 milliseconds = 5 seconds
     </script>
-</body>
-</html>
+
+
+</!doctype>
