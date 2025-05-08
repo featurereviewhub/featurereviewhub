199089bb6ad284f4f7d264e7d81107fca8581d2f
featurereviewhub <ABDESSAMADOSSEILI2003@gmail.com>
2025-05-08 19:24:05 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,21 @@
+ import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to your desired URL
+    window.location.href = 'https://fhfnff.blogspot.com/';
+
+    // Optionally, you can use router.push() instead for client-side routing
+    // router.push('https://featurereviewhub.com/');
+  }, []);
+
+  return (
+    <div>
+      <p>Redirecting...</p>
+      {/* You can optionally add a message or spinner here */}
+    </div>
+  );
+}
