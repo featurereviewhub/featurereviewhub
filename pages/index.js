deae53b2338d1a010b7ca0a293a741600d405aef
featurereviewhub <ABDESSAMADOSSEILI2003@gmail.com>
2025-05-11 02:04:18 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Redirect Example</title>
-</head>
-<body>
-    <img id="tiktok-logo" src="https://i.ibb.co/5MMGFNf/1000-F-344600545-4j-FBe-Ak5l8-Kug2-Uyxq-OEoop8p1-I0v1dp-removebg-preview.png" alt="TikTok Logo" />
-    <p id="redirect-text">Being Redirected ...</p>
-    <a href="https://www.google.com" id="openLink">Click Me to Open in Chrome (Android) or Safari (iOS)</a>
 
-    <script>
-        function openLinkBasedOnDevice(url) {
-            var userAgent = navigator.userAgent;
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
-            if (/(android)/i.test(userAgent)) {
-                // If the device is Android, open the link in Chrome
-                window.location.href = 'intent://' + url.substring(url.indexOf("://") + 3) + '#Intent;scheme=http;package=com.android.chrome;end;';
-            } else if (/(iphone|ipad|ipod)/i.test(userAgent)) {
-                // If the device is iOS, open the link in Safari
-                window.location.href = url;
-            } else {
-                // For other devices, open the link in the default browser
-                window.open(url, '_blank');
-            }
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="HEY!" />
+        <p className="description">
+          Get started shopping at 
+          <a href="https://featurereviewhub.com" className="link">
+            FEATUREREVIEWHUB
+            <svg className="arrow" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M13 4L20 12L13 20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M4 12H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+          </a>
+        </p>
+      </main>
+
+      <Footer />
+      <style jsx>{`
+        .link {
+          display: inline-flex;
+          align-items: center;
         }
-
-        // Automatically execute the function after a 5-second delay
-        setTimeout(function () {
-            var url = 'https://featurereviewhub.com/';
-            openLinkBasedOnDevice(url);
-        }, 5000); // 5000 milliseconds = 5 seconds
-    </script>
-</body>
-</html>
+        .arrow {
+          margin-left: 5px;
+        }
+      `}</style>
+    </div>
+  )
+}
